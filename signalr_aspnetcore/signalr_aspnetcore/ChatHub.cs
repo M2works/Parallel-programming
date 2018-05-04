f3e0605e104817092d93622f0ada076f2ff96f78
Maciej Momot <mcmomot96@gmail.com>
2018-05-04 20:55:45 +0200

    Working prototype of SignalR added

---
+++ signalr_aspnetcore/signalr_aspnetcore/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace signalr_aspnetcore
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.InvokeAsync("broadcastMessage", name, message);
+        }
+        public override Task OnConnectedAsync()
+        {
+            Clients.All.InvokeAsync("broadcastMessage", "system", $"{Context.ConnectionId} joined the conversation");
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(System.Exception exception)
+        {
+            Clients.All.InvokeAsync("broadcastMessage", "system", $"{Context.ConnectionId} left the conversation");
+            return base.OnDisconnectedAsync(exception);
+        }
+
+    }
+}
